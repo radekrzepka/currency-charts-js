f456fc0e3cecc5260278548c40356d52186cbbdf
Radosław Rzepka <radek.rz.123@gmail.com>
2022-04-16 17:31:30 +0200

    added charts

--- js/charts/currencyCharts.js
+++ js/charts/currencyCharts.js
@@ -1,19 +1,58 @@
-import { getAllRates, getCurrencies } from "../apiServices.js";
+import { getAllRates, getCurrencies, getCurrencyRates } from "../apiServices.js";
 
 async function createTable() {
 	const table = document.querySelector(".currencyTable");
 	const allRates = await getAllRates();
 	const allCurrencies = await getCurrencies();
-	allRates.EUR = 1;
 	for (const currency in allRates) {
 		const newRow = document.createElement("tr");
 		newRow.innerHTML = `
             <td>${currency} ${allCurrencies[currency]}</td>
-            <td>${Math.round((allRates[currency] / allRates.PLN) * 100) / 100}</td>
-            <td>Pokaż wykres</td>
+            <td>${Math.round(allRates[currency] * 100) / 100}</td>
+            <td class = 'showChart' id='${currency}'>Pokaż wykres</td>
         `;
 		table.append(newRow);
 	}
+	return document.querySelectorAll(".showChart");
 }
 
-createTable();
+const showChartButtons = await createTable();
+
+async function printChart(currency) {
+	const historicalRates = await getCurrencyRates(currency);
+	const dates = [];
+	const rates = [];
+	const currencyName = document.querySelector("#currencyName");
+	currencyName.textContent = currency;
+	for (const date in historicalRates) {
+		dates.push(date);
+		rates.push(historicalRates[date][currency]);
+	}
+
+	const ctx = document.getElementById("chart").getContext("2d");
+	const myChart = new Chart(ctx, {
+		type: "line",
+		data: {
+			labels: dates,
+			datasets: [
+				{
+					label: "Wartość",
+					data: rates,
+					backgroundColor: ["rgb(0,0,0)"],
+					borderColor: ["rgb(0,0,0)"],
+					borderWidth: 1,
+				},
+			],
+		},
+	});
+	return new Promise(resolve => resolve(myChart));
+}
+
+let actualChart = await printChart("PLN");
+
+showChartButtons.forEach(button => {
+	button.addEventListener("click", async () => {
+		actualChart.destroy();
+		actualChart = await printChart(button.id);
+	});
+});
