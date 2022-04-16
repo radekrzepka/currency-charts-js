ac5cd51884c7dfeff085ffc0adb21a8c65630057
ziebamikolaj <ziebamikolaj03@gmail.com>
2022-04-16 17:12:38 +0200

    checking if number is legal, few changes

--- js/converter/currencyConverter.js
+++ js/converter/currencyConverter.js
@@ -10,16 +10,32 @@ async function createSelect() {
 		document.querySelector("#currenciesSelectFirst").appendChild(newCurrency1);
 		document.querySelector("#currenciesSelectSecond").appendChild(newCurrency2);
 	}
+	calculator();
 }
 
 createSelect();
 
-document.querySelector("#calcButton").addEventListener("click", calculator);
+const inputNumber = document.querySelector("#currenciesNumber");
+inputNumber.addEventListener("input", replaceWrongCharacters);
+
+async function replaceWrongCharacters() {
+	let result = inputNumber.value.match(/([0-9.])/g);
+	if (result != null) result = result.join("");
+	inputNumber.value = result;
+}
+
+const inputCurrencies = document.querySelectorAll("[name='currencies']");
+for (let x = 0; x < inputCurrencies.length; x++) {
+	inputCurrencies[x].addEventListener("change", calculator);
+}
+
 async function calculator() {
-	let firstValue = document.querySelector("#currenciesSelectFirst").value;
-	let secondValue = document.querySelector("#currenciesSelectSecond").value;
-	const allRates = await getAllRates();
+	let firstCurr = document.querySelector("#currenciesSelectFirst").value;
+	let secondCurr = document.querySelector("#currenciesSelectSecond").value;
+	let numberCurr = document.querySelector("#currenciesNumber").value;
+	if (numberCurr == "") numberCurr = "0";
+	const allRates = await getAllRates(currenciesNumber);
 	allRates.EUR = 1;
-	let conversion = Math.round((allRates[secondValue] / allRates[firstValue]) * 100) / 100;
-	document.querySelector("#calcResult").innerHTML = `1 ${firstValue} = ${conversion} ${secondValue}`;
+	let conversion = (numberCurr * Math.round((allRates[secondCurr] / allRates[firstCurr]) * 100)) / 100;
+	document.querySelector("#calcResult").innerHTML = `${numberCurr} ${firstCurr} = ${conversion} ${secondCurr}`;
 }
